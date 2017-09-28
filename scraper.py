62d589b8986a27d2385051a27cdc9d5146e06083
Jessica_Kam <jessica.c.kam@berkeley.edu>
2017-09-28 10:36:52 -0700

    scraper works, but should be cleaned up

--- scraper.py
+++ scraper.py
@@ -4,13 +4,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from random import randint
 from time import sleep
+from difflib import SequenceMatcher
 
 class Scraper():
 	"""
 	Searches AllMenus.com for a restaurant's menu items and corresponding descriptions.
 	"""
 	def __init__(self, restuarant_name, restuarant_addr):
-		self.site = 'allmenus.com'
+		self.site = 'https://www.allmenus.com'
 		self.wait_time = 60
 		self.restuarant_name = restuarant_name
 		self.restuarant_addr = restuarant_addr
@@ -44,14 +45,39 @@ class Scraper():
 
 	def selectRestaurantFromResults(self):
 		print('Selecting desired restuarant from search results...')
+		"""
 		lst_results = self.browser.find_elements(By.CSS_SELECTOR, 'h4.name')
 		restuarant = None
 		for result in lst_results:
-			if result.get_attribute('inner-html') == self.restuarant_name:
-				restuarant = result
+			print('RESULT')
+			print(result)
+			print('INNER_HTML')
+			print(result.get_attribute('inner-html'))
+			if not isinstance(result, type(None)): #and (result.get_attribute('inner-html').lower() == self.restuarant_name.lower()):
+				#how modify so just if similar enough
+				if result.get_attribute('inner-html') and (self.similar(result.get_attribute('inner-html').lower(), self.restuarant_name.lower()) > 0.8):
+					restuarant = result
+					break
+		"""
+
+		lst_results = self.browser.find_elements(By.CSS_SELECTOR, 'h4.name a')
+		restuarant_href = None
+		for result in lst_results:
+			print('RESULT')
+			print(result)
+			print('href')
+			print(result.get_attribute('href'))
+			if not isinstance(result, type(None)): #and (result.get_attribute('inner-html').lower() == self.restuarant_name.lower()):
+				#how modify so just if similar enough
+				possible_href = result.get_attribute('href')
+				if possible_href and self.restuarant_name.lower().replace(' ', '-') in possible_href:
+					restuarant_href = possible_href
+					break
+		
 		print('Sleeping...')
 		sleep(randint(self.sleep_min, self.sleep_max))
-		restuarant.click() #
+		#restuarant.click() #
+		self.browser.get(restuarant_href)
 
 
 		##select actual restuarant
@@ -60,10 +86,19 @@ class Scraper():
 		#then click
 		#sleep
 
+	def similar(self, a, b):
+		return SequenceMatcher(None, a, b).ratio()
+
 	def pairMenuItemsAndDescriptions(self):
 		print('Pairing menu items with their corresponding descriptions')
-		menu_items = self.browser.find_elements(By.CLASS_NAME, 'item-title')
-		item_descriptions = self.browser.find_elements(By.CSS_SELECTOR, 'p.description')
+		menu_items = []
+		resulting_menu_items = self.browser.find_elements(By.CLASS_NAME, 'item-title')
+		item_descriptions = []
+		resulting_item_descriptions = self.browser.find_elements(By.CSS_SELECTOR, 'p.description')
+		for i in range(len(resulting_menu_items)): #assert same length
+			#not work: inner-html, text
+			menu_items.append(resulting_menu_items[i].text)#.get_attribute('value'))
+			item_descriptions.append(resulting_item_descriptions[i].text) #.get_attribute('value'))
 		return zip(menu_items, item_descriptions)
 
 		#select menu items by css selector (class): item-title
@@ -80,10 +115,13 @@ scraper = Scraper(restuarant_name, restuarant_addr)
 scraper.startBrowser()
 scraper.searchRestuarant()
 scraper.selectRestaurantFromResults()
-menu_dict = scraper.pairMenuItemsAndDescriptions()
+menu = scraper.pairMenuItemsAndDescriptions()
 scraper.closeBrowser()
 
 print(restuarant_name)
 print(restuarant_addr)
-for k, v in menu_dict:
-	print(k, v)
+count = 0
+for item, description in menu:
+	print(item, description)
+	count += 1
+print(count)
