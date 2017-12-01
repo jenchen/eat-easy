598b4295b0106fa628a4cc4f2c6711c9cf6ff928
Jessica_Kam <jessica.c.kam@berkeley.edu>
2017-12-01 00:08:09 -0800

    scraper unchecks box and updated yelp_filter_queries

--- scraper.py
+++ scraper.py
@@ -12,21 +12,29 @@ class Scraper():
 	"""
 	def __init__(self, restuarant_name, restuarant_addr):
 		self.site = 'https://www.allmenus.com'
-		self.wait_time = 60
-		self.restuarant_name = restuarant_name
-		self.restuarant_addr = restuarant_addr
-		self.sleep_min = 3
-		self.sleep_max = 7
+		self.wait_time = 5 #60
+		self.restuarant_name = restuarant_name ##
+		self.restuarant_addr = restuarant_addr ##
+		self.sleep_min = 0 #3
+		self.sleep_max = 1 #7
 
 	def startBrowser(self):
 		self.browser = webdriver.PhantomJS()
 		self.browser.implicitly_wait(self.wait_time)
 		self.browser.get(self.site)
-		##figure out how to deal with only online orders
-
 
 	def searchRestuarant(self):
+		print(">>>>>>>>>>")
 		print('Searching for desired restuarant...')
+		#check_box = self.browser.find_element(By.CSS_SELECTOR, "div.s-checkbox-group.only-online-experiment-checkbox span.s-checkbox-filler")
+		check_box = self.browser.find_element(By.CSS_SELECTOR, "input#online-ordering-checkbox.s-checkbox-input")
+		print("CHECK_BOX VALUE (should be 'online-ordering'): ", check_box.get_attribute('value'))
+		check_box.click()
+
+		##if check_box.is_selected():
+    	#check_box.click()
+		##search_field = self.browser.find_element(By.CSS_SELECTOR, 'form#am-search-form-desktop input#header-get-address')
+		##print("SEARCH_FIELD: ", search_field.get_attribute('placeholder'))
 		search_field = self.browser.find_element(By.ID, 'get-address')
 		search_field.clear()
 		search_field.send_keys(self.restuarant_addr)
@@ -59,25 +67,30 @@ class Scraper():
 					restuarant = result
 					break
 		"""
+		num_results_found = self.browser.find_element(By.CSS_SELECTOR, 'h1')
+		num_results_found = num_results_found.text.split()[0] #get_attribute('inner-html')
+		print(str(num_results_found) + " NUMBER OF RESULTS FOUND")
 
-		lst_results = self.browser.find_elements(By.CSS_SELECTOR, 'h4.name a')
-		restuarant_href = None
-		for result in lst_results:
-			print('RESULT')
-			print(result)
-			print('href')
-			print(result.get_attribute('href'))
-			if not isinstance(result, type(None)): #and (result.get_attribute('inner-html').lower() == self.restuarant_name.lower()):
-				#how modify so just if similar enough
-				possible_href = result.get_attribute('href')
-				if possible_href and self.restuarant_name.lower().replace(' ', '-') in possible_href:
-					restuarant_href = possible_href
-					break
-		
-		print('Sleeping...')
-		sleep(randint(self.sleep_min, self.sleep_max))
-		#restuarant.click() #
-		self.browser.get(restuarant_href)
+		if int(num_results_found) > 0:
+
+			lst_results = self.browser.find_elements(By.CSS_SELECTOR, 'h4.name a')
+			restuarant_href = None
+			for result in lst_results:
+				print('RESULT')
+				print(result)
+				print('href')
+				print(result.get_attribute('href'))
+				if not isinstance(result, type(None)): #and (result.get_attribute('inner-html').lower() == self.restuarant_name.lower()):
+					#how modify so just if similar enough
+					possible_href = result.get_attribute('href')
+					if possible_href and self.restuarant_name.lower().replace(' ', '-').replace("'", "-") in possible_href:
+						restuarant_href = possible_href
+						break
+			
+			print('Sleeping...')
+			sleep(randint(self.sleep_min, self.sleep_max))
+			#restuarant.click() #
+			self.browser.get(restuarant_href)
 
 
 		##select actual restuarant
@@ -109,8 +122,8 @@ class Scraper():
 		if self.browser:
 			self.browser.quit()
 
-restuarant_name = 'House of Curries'
-restuarant_addr = '2520 Durant Ave, Berkeley, CA, 94704'
+restuarant_name = "Matt's Big Breakfast" #'House of Curries'
+restuarant_addr = "801 N 1st St, Phoenix, AZ" #'2520 Durant Ave, Berkeley, CA, 94704'
 scraper = Scraper(restuarant_name, restuarant_addr)
 scraper.startBrowser()
 scraper.searchRestuarant()
@@ -122,6 +135,7 @@ print(restuarant_name)
 print(restuarant_addr)
 count = 0
 for item, description in menu:
-	print(item, description)
+	print(item)
+	print(description)
 	count += 1
 print(count)
